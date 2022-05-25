a22a4ce9dfd31b65f780066596e0d52b034f3a18
emma-junior <emnzboy31@gmail.com>
2022-05-25 10:22:18 +0100

    slight-commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import {FaCaretDown} from 'react-icons/fa'
 import {FaBars} from 'react-icons/fa'
 import {FaTimes} from 'react-icons/fa'
 import { Link } from "react-router-dom";
