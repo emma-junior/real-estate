48f15903246efb1fed8961fc0409339756b73b9c
emma-junior <emnzboy31@gmail.com>
2022-10-20 23:42:34 +0100

    modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="">
       <Navbar />
-      <div className="bg-cover-pic bg-cover bg-no-repeat bg-center h-screen" >
+      <div className="bg-cover-pic bg-cover bg-no-repeat bg-center bg-[#ccc] h-screen" >
         <h2 className="lg:text-5xl text-2xl font-bold text-white lg:w-3/5 w-4/5 lg:pl-10 pl-5 lg:pt-16 pt-24">
           Your No.1 plug for all your housing needs anywhere in Nigeria
         </h2>
@@ -16,7 +16,7 @@ const Home = () => {
       <div className="bg-[#111] py-10 text-white text-center">
         <h2 className="text-3xl text-[#603276] mb-8">Mercury Homes</h2>
         <h2 className="lg:text-4xl text-2xl font-bold w-4/5 lg:ml-24 ml-8 mb-6">
-          The Largest And Biggest Real Estate Company For Home Owners And Buyersss
+          The Largest And Biggest Real Estate Company For Home Owners And Buyers
           In Nigeria
         </h2>
         <h2 className="text-2xl mb-4">We Make The Incredible Affordable</h2>
